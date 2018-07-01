07c058f5e167a25f62202f257b1f92b5628a770d
Ansontheprogrammer <ansonervin@yahoo.com>
2018-07-01 09:12:30 -0400

    I'm having an issue with google default api credentials, I copied the storage function for the credentials to the code, however there still seems to be an issue

--- Speech to text.js
+++ Speech to text.js
@@ -1,3 +1,27 @@
+// Imports the Google Cloud client library.
+const Storage = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+
+// Makes an authenticated API request.
+storage
+  .getBuckets()
+  .then((results) => {
+    const buckets = results[0];
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+
+// [START speech_quickstart]
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
@@ -39,3 +63,4 @@ client
   .catch(err => {
     console.error('ERROR:', err);
   });
+// [END speech_quickstart]
