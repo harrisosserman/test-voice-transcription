b4c10b24d7d5f3b21c6e6c5117466c3ac091f995
Anson <ansonervin@yahoo.com>
2018-07-24 23:18:59 -0400

    Everything is all fixed. I was having trouble with transcribing any files that weren't mono wav files, so I believe that may have been our issue.

---
+++ long_example.js
@@ -0,0 +1,48 @@
+//set the credntials.json file
+process.env.GOOGLE_APPLICATION_CREDENTIALS = 'credentials.json';
+
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+const gcsUri = '';
+const encoding = 'LINEAR16';
+const sampleRateHertz = 22050;
+const languageCode = 'en-US';
+
+const config = {
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  languageCode: languageCode,
+};
+
+const audio = {
+  uri: gcsUri,
+};
+
+const request = {
+  config: config,
+  audio: audio,
+};
+
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
+client
+  .longRunningRecognize(request)
+  .then(data => {
+    const operation = data[0];
+    // Get a Promise representation of the final result of the job
+    return operation.promise();
+  })
+  .then(data => {
+    const response = data[0];
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
---
+++ short_example.js
@@ -0,0 +1,68 @@
+//set the credntials.json file
+process.env.GOOGLE_APPLICATION_CREDENTIALS = 'credentials.json';
+
+// Imports the Google Cloud client library.
+const Storage = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = new Storage();
+
+// Makes an authenticated API request.
+storage
+  .getBuckets()
+  .then((results) => {
+    const buckets = results[0];
+
+    console.log('Buckets:');
+    buckets.forEach((bucket) => {
+      console.log(bucket.name);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+
+// [START speech_quickstart]
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+// The name of the audio file to transcribe
+const fileName = './sample.wav';
+
+// Reads a local audio file and converts it to base64
+const file = fs.readFileSync(fileName);
+const audioBytes = file.toString('base64');
+
+// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+const audio = {
+  content: audioBytes,
+};
+const config = {
+  encoding: 'LINEAR16',
+  languageCode: 'en-US',
+};
+const request = {
+  audio: audio,
+  config: config,
+};
+
+// Detects speech in the audio file
+client
+  .recognize(request)
+  .then(data => {
+    const response = data[0];
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+// [END speech_quickstart]
