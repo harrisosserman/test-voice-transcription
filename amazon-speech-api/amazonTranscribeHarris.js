3f97ef9f52eacde2c8b9bf1f135ce78dbd07a4f5
Anson <ansonervin@yahoo.com>
2018-08-11 14:00:35 -0400

    Left an error after polishing

--- amazon-speech-api/amazonTranscribeHarris.js
+++ amazon-speech-api/amazonTranscribeHarris.js
@@ -3,6 +3,14 @@ const AWS = require('aws-sdk');
 AWS.config.update({region:'us-east-1'});
 const transcribeservice = new AWS.TranscribeService();
 
+const params = {
+  file: 'https://s3.amazonaws.com/talk-long-example/REC-40.-Driving.mp3',
+  format: 'mp3',
+  jobName: 'transcribe-anson-voice-recording-test-11',
+  outputBucketName: 'talk-long-example',
+  outputJson:'https://s3.amazonaws.com/talk-long-example/transcribe-anson-voice-recording-test-10.json',
+};
+
 function createVocabulary() {
   const params = {
     LanguageCode: 'en-US',
@@ -31,47 +39,36 @@ function startTranscriptionJob(transcriptionObject) {
       // MaxSpeakerLabels: 0,
       // ShowSpeakerLabels: false
     }
-	}
+  }
     transcribeservice.startTranscriptionJob(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else     console.log(data);           // successful response
-  		});
-  	};
+      });
+    };
 
 function getTranscriptionJob(outputJson){
-	request(outputJson, { json: true }, (err, res, body) => {
-	 	if (err) { return console.log(err); }
-	  	console.log(body.results.transcripts);
-	});
+  request(outputJson, { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+      console.log(body.results.transcripts);
+  });
 }
 
-function transcribe(transcriptionObject={}, newTranscribe=true) {
-	const params = {
-    file: 'https://s3.amazonaws.com/talk-long-example/REC-40.-Driving.mp3',
-    format: 'mp3',
-    jobName: 'transcribe-anson-voice-recording-test-11',
-    outputBucketName: 'talk-long-example',
-    outputJson:'https://s3.amazonaws.com/talk-long-example/transcribe-anson-voice-recording-test-10.json',
-  }
-	if (transcriptionObject != {}){
-		this.transcriptionObject = transcriptionObject;
-	} else {
-		this.transcriptionObject = params;
-	}
-	if (newTranscribe == false){
-		startTranscriptionJob(transcriptionObject);
-	}
-  this.transcribeComplete(params);	
+function transcribe(transcriptionObject, newTranscribe=true) {
+  this.transcriptionObject = transcriptionObject;
+  if (newTranscribe == false){
+    startTranscriptionJob(transcriptionObject);
+  } 
 }
 transcribe.prototype.getProgress = ()=>{
-		transcribeservice.listTranscriptionJobs({Status:'COMPLETED'}, (err, results)=> {console.log(err, results)});
-		transcribeservice.listTranscriptionJobs({Status:'IN_PROGRESS'}, (err, results)=> {console.log(err, results)});
-		transcribeservice.listTranscriptionJobs({Status:'Failed'}, (err, results)=> {console.log(err, results)})
-	}
+    transcribeservice.listTranscriptionJobs({Status:'COMPLETED'}, (err, results)=> {console.log(err, results)});
+    transcribeservice.listTranscriptionJobs({Status:'IN_PROGRESS'}, (err, results)=> {console.log(err, results)});
+    transcribeservice.listTranscriptionJobs({Status:'Failed'}, (err, results)=> {console.log(err, results)})
+  }
 //Requires global params arg, and returns string containing transcription
-transcribe.prototype.transcribeComplete = (transcriptionObject)=>{
-		return getTranscriptionJob(transcriptionObject.outputJson);
-	}
-let transcription = new transcribe();
+transcribe.prototype.transcribeComplete = (obj)=>{
+    return getTranscriptionJob(obj.outputJson);
+  }
+let transcription = new transcribe(params);
 // // Uncomment to the list the progression of all transcription jobs
 // transcription.getProgress();
+transcription.transcribeComplete(params);
